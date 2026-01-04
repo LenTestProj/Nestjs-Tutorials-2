f614235f959fb05fb7ff2bf9b78615a841623d1d
LenTestProj <lennfernz2@gmail.com>
2026-01-04 18:09:04 +0530

    small change

--- Anson_Nestjs_Mongodb/nestjs-mongodb/src/app.module.ts
+++ Anson_Nestjs_Mongodb/nestjs-mongodb/src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
-import {MongooseModule} from "@nestjs/mongoose"
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [ 
+    ConfigModule.forRoot({
+        isGlobal:true,
+    }), 
+    // MongooseModule.forRoot(process.env.MONGO_URI)
+    MongooseModule.forRootAsync({
+        inject:[ConfigService],
+        useFactory:(config:ConfigService) => ({
+            uri: config.get("MONGO_URI"),
+            connectionFactory:(connection)=>{
+                console.log("Mongodb connected");
+                return connection
+            }
+        }),
+    }),
+  ], 
   controllers: [],
   providers: [],
 })
